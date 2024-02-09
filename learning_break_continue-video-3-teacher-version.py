9a33ded5a55dae27e2542a01d100c30d89a86157
gato365 <immanuelwilliams@gato365.com>
2024-02-09 07:36:07 -0800

    Video 3 teacher notes

---
+++ learning_break_continue-video-3-teacher-version.py
@@ -0,0 +1,17 @@
+
+
+i = 0
+while i < 5:
+    if i == 3:
+        break
+    print(i)
+    i += 1
+    
+
+i = 0
+while i < 5:
+    if i == 3:
+        i += 1
+        continue
+    print(i)
+    i += 1
