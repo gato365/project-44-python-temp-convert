1246b812a5b872184110c45929edee7ab6fe9004
gato365 <immanuelwilliams@gato365.com>
2024-02-10 06:45:10 -0800

    Added student version of the code

---
+++ learning_break_continue-video-3.py
@@ -0,0 +1,22 @@
+# i = 1
+# print(i)
+# i += 1 ## i = i + 1
+# print(i)
+
+
+
+# i = 0 
+# while i < 5:
+#     if i == 2:
+#         break
+#     print(i)
+#     i += 1
+
+
+i = 0 
+while i < 5:
+    if i == 1:
+        i += 1
+        continue
+    print(i)
+    i += 1
